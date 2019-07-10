1079f8d25cdcf0abd0a7fa340769d217eed3a727
Roshan <roshan.reji92@gmail.com>
2019-07-09 22:13:03 -0400

    Added insert and delete functionality to the Linked List

--- src/dataStructures/LinkedList.java
+++ src/dataStructures/LinkedList.java
@@ -3,55 +3,78 @@ import java.util.*;
 
 public class LinkedList {
 	static Node head;
-	
+	static Node tail;
+	static int size;
 	public static void main (String[] args) {
 		System.out.println("Enter the size of Linked List");
 		Scanner sc = new Scanner(System.in);
-		int size = sc.nextInt();
+		size = sc.nextInt();
 		addValue(size);
 		selectOptions();
 		sc.close();
 	}
 	
 	public static void selectOptions() {
-		System.out.println("Select the options from below to perform your task");
-		System.out.println(	"1. To print the Linked List"+'\n'+
-							"2. To add a new element to the Linked List"+'\n'+
-							"3. To delete an element from the Linked List");
-		System.out.println();
 		Scanner sc1 = new Scanner(System.in);
-		int option = sc1.nextInt();
-		int position,value;
-		switch (option){
-			case 1:
-				traverseLinkedList();
+		while (true) {
+			System.out.println ("Do you wish to continue?");
+			System.out.println("Press y or n : ");
+			char yesno = sc1.next().charAt(0);
+			
+			if(yesno == 'n' || yesno == 'N') {
+				System.out.println("Exiting...");
 				break;
-			case 2:
-				System.out.println("Enter the value and the position where the new entry is to be inserted");
-				System.out.print("Value : ");
-				value = sc1.nextInt();
+			}
+			else if (yesno == 'y' || yesno == 'Y') {
+				System.out.println("Select the options from below to perform your task");
+				System.out.println(	"1. To print the Linked List"+'\n'+
+									"2. To add a new element to the Linked List"+'\n'+
+									"3. To delete an element from the Linked List");
 				System.out.println();
-				System.out.print("Position : ");
-				position = sc1.nextInt();
-				System.out.println();
-				System.out.println(value+" "+position);
-				//addNewElement(value,position);
+				int option = sc1.nextInt();
+				int position,value;
+				switch (option){
+					case 1:
+						traverseLinkedList();
+						break;
+					case 2:
+						System.out.println("Enter the value and the position where the new entry is to be inserted");
+						System.out.print("Value : ");
+						value = sc1.nextInt();
+						System.out.println();
+						System.out.print("Position : ");
+						position = sc1.nextInt();
+						System.out.println();
+						System.out.println("Linked List before inserting the element");
+						traverseLinkedList();
+						insertValue(value,position);
+						System.out.println("Linked List after inserting the element");
+						traverseLinkedList();
+						break;
+					case 3:
+						System.out.print("Enter the position from where the value is to be deleted : ");
+						position = sc1.nextInt();
+						System.out.println("Linked List before deleting the element");
+						traverseLinkedList();
+						deleteValue(position);
+						System.out.println("Linked List after deleting the element");
+						traverseLinkedList();
+						break;
+					default:
+						System.out.println("Please Enter the correct option as mentioned above");
+						break;
+				}
+			}
+			else {
+				System.out.println("Invalid Entry");
 				break;
-			case 3:
-				System.out.print("Enter the position from where the value is to be deleted : ");
-				position = sc1.nextInt();
-				System.out.println();
-				//deleteElement(position);
-				break;
-			default:
-				System.out.println("Please Enter the correct option as mentioned above");
-				break;
+			}
 		}
-		sc1.close();
+		//sc1.close();
 	}
 	
 	public static void addValue(int size) {
-		LinkedList llist = new LinkedList();
+		//LinkedList llist = new LinkedList();
 		System.out.println("Enter the head of the Linked List");
 		Scanner sc2 = new Scanner (System.in);
 		int hd = sc2.nextInt();
@@ -68,6 +91,7 @@ public class LinkedList {
 			current.next = temp;
 			current = temp;
 		}
+		tail = current;
 		//sc2.close();
 	}
 	
@@ -79,5 +103,55 @@ public class LinkedList {
 			current = current.next;
 		}
 		System.out.println();
+	}
+	
+	public static void insertValue(int value, int position) {
+		Node current = head;
+		if (position==(size+1)) {
+			current = tail;
+			Node temp = new Node(value);
+			current.next = temp;
+			tail = temp;
+		}
+		
+		else if (position == 1) {
+			Node temp = new Node(value);
+			head = temp;
+			head.next = current;
+		}
+		else {
+			int counter = 1;
+			while (counter<position) {
+				if(counter == (position-1)) {
+					Node temp = current.next;
+					Node newValue = new Node(value);
+					current.next = newValue;
+					newValue.next = temp;
+				}
+				current = current.next;
+				counter++;
+			}
+		}
+	}
+	
+	public static void deleteValue(int position) {
+		Node current = head;
+		if(position == 1) {
+			head = current.next;
+		}
+		else {
+			int counter = 1;
+			while(counter<position) {
+				if(counter == position-1) {
+					current.next = current.next.next;
+					if(counter == size - 1) {
+						tail = current;
+					}
+				}
+				current = current.next;
+				counter++;
+			}
+			
+		}
 	}
 }
